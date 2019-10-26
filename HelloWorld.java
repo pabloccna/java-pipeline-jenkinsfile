2163f44d7b17607f457355284d27aafc039f6c97
Pablo <pablo@Pablos-MacBook-Pro.local>
2019-10-26 19:29:13 -0300

    ...

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,10 @@ public class HelloWorld {
  
 	public static void main(String[] args) {
 		int i=66;
-		S
 		System.out.println("----------------------------------------------------------");
 		System.out.println("Hola Mundo ......");
 		System.out.println("el valor de i: "+i);
 		System.out.println("Ahora hay un: Jenkinsfile + HelloWorld.java");
 		System.out.println("----------------------------------------------------------");
 	}
- 
 }

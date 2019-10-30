5f1fac5fdad4a585353a7e449a04006f03e43dea
Pablo <pablo@Pablos-MBP.utopia.net>
2019-10-29 23:44:44 -0300

    ...

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
 		System.out.println("Hola Mundo ......");
 		System.out.println("el valor de i: "+i);
 		System.out.println("Ahora hay un: Jenkinsfile + HelloWorld.java");
-		System.out.println("----------------------------------------------------------");
+		System.out.println("-----------------------------------------------------------");
 	}
 }

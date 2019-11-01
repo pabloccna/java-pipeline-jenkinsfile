ad4aa021402892e0a7a64d15c75155e560fcd3c5
pabloccna <52111134+pabloccna@users.noreply.github.com>
2019-10-31 21:01:15 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		int i=66;
 		System.out.println("----------------------------------------------------------");
-		System.out.println("Hola Mundo ......");
+		System.out.println("Hola Mundo puto......");
 		System.out.println("el valor de i: "+i);
 		System.out.println("Ahora hay un: Jenkinsfile + HelloWorld.java");
 		System.out.println("-----------------------------------------------------------");

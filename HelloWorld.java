4f2b28194d7e9b095f7a35753f90abb2356eb882
pabloccna <52111134+pabloccna@users.noreply.github.com>
2020-03-28 02:17:47 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
  
 	public static void main(String[] args) {
-		int i=666;
+		int i=99;
 		System.out.println("----------------------------------------------------------");
 		System.out.println("Hola Mundo...... y entendiendo el webhook");
 		System.out.println("el valor de i: "+i);

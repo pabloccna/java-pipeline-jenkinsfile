068a4b70c31e2f6563b9b2da0a3d0a8924ae8596
pabloccna <52111134+pabloccna@users.noreply.github.com>
2020-03-30 21:29:54 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ provider "aws" {
 ####
 output "public_ip" {
 	description = "La IP pub de..."
- 	value = "${aws_instance.public_ip}"
+ 	value = "${aws_instance.example.public_ip}"
 
 resource "aws_instance" "example" {
   ami           = "ami-0fc61db8544a617ed"

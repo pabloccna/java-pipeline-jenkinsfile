b7c8016d5e87f1be2705a4701ab97591fec98ef1
pabloccna <52111134+pabloccna@users.noreply.github.com>
2020-03-30 21:25:31 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,29 @@ provider "aws" {
   profile    = "default"
   region     = "us-east-1"
 }
+####
+# output
+####
+output "public_ip-2" {
+	description = "La IP pub de..."
+ 	value = "${aws_instance.public_ip}"
 
 resource "aws_instance" "example" {
   ami           = "ami-0fc61db8544a617ed"
   instance_type = "t2.nano"
+  user_data = "#/bin/bash\yum update -y\yum install httpd -y\service httpd start"
+	vpc_security_group_ids  = ["sg-01d0fb5d93537bded","sg-0f9669d5985e6ccac"]
+	
+tags= {
+	Name = "AR-FE-1"
+	Ceco = "CSC95TEC"
+
+	}
+
+volume_tags= {
+	Name = "AR-FE-1"
+	Ceco = "CSC95TEC"
+  
 }
+
+
